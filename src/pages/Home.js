bc42ebd28f468c8d72c315d23176c703394f92b9
Jorge Vargas <jvargas18@ucol.mx>
2021-04-14 01:47:48 -0500

    Add pokemon list

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import api from "../Api";
+import PokemonList from "../components/PokemonList";
 
 function Home() {
   const [state, setState] = useState({
@@ -25,22 +26,7 @@ function Home() {
 
   return (
     <div className="container">
-      <div className="row">
-        {state.data?.map((pokemon) => (
-          <div className="col-3 my-3">
-            <div className="card">
-              <img
-                src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
-                  pokemon.url.match(/\/pokemon\/(\d+)\//)[1]
-                }.png`}
-                className="card-img-top w-50 mx-auto d-block"
-                alt="..."
-              />
-              <div className="card-body">{pokemon.name}</div>
-            </div>
-          </div>
-        ))}
-      </div>
+      <PokemonList pokemonList={state.data} />
     </div>
   );
 }
